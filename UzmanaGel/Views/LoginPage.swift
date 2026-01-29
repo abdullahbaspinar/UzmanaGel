6b3f7bf5f82a3662d0bb9709180e12aca068ac57
abaspnr <117168519+abaspnr@users.noreply.github.com>
2026-01-29 20:29:08 +0300

    splash and login ui

---
+++ UzmanaGel/Views/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  UzmanaGel
+//
+//  Created by Abdullah B on 29.01.2026.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginPage()
+}
---
+++ UzmanaGel/Views/PreViewScreen.swift
@@ -0,0 +1,18 @@
+//
+//  PreViewScreen.swift
+//  UzmanaGel
+//
+//  Created by Abdullah B on 29.01.2026.
+//
+
+import SwiftUI
+
+struct PreViewScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PreViewScreen()
+}
