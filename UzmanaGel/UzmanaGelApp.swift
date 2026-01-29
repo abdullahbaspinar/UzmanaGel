c7994f765a0449adf85fcfd4a52a25709eab8b0f
abaspnr <117168519+abaspnr@users.noreply.github.com>
2026-01-29 14:33:55 +0300

    Initial Commit

---
+++ UzmanaGel/UzmanaGelApp.swift
@@ -0,0 +1,17 @@
+//
+//  UzmanaGelApp.swift
+//  UzmanaGel
+//
+//  Created by Abdullah B on 29.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct UzmanaGelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
