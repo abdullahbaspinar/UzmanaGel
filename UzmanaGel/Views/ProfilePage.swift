54bde8a70d9b3f6b7356b316b93d7031d1885d0b
abaspnr <117168519+abaspnr@users.noreply.github.com>
2026-02-10 16:26:21 +0300

    ProfilePageUI

---
+++ UzmanaGel/Views/ProfilePage.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePage.swift
+//  UzmanaGel
+//
+//  Created by Abdullah B on 10.02.2026.
+//
+
+import SwiftUI
+
+struct ProfilePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePage()
+}
