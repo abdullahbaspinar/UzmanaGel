43d28ec9dcb6efd7dfcd99ba7bc0dc00e4307c00
abaspnr <117168519+abaspnr@users.noreply.github.com>
2026-02-17 20:19:55 +0300

    firebase

---
+++ UzmanaGel/Views/FavoritesPage.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesPage.swift
+//  UzmanaGel
+//
+//  Created by Abdullah B on 12.02.2026.
+//
+
+import SwiftUI
+
+struct FavoritesPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesPage()
+}
